0a6cb44a20be04c7568dc67dd4e4b74d10af0f60
camargobriel <camargobriel@gmail.com>
2026-02-06 14:50:44 -0300

    habilitei o auto save

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 console.log("Minha primeira linha de codigo");
 console.log("Minha segunda linha de codigo");
 console.log("Minha terceira linha de codigo");
+
+console.log("demorei pra entender que tinha que habilitar o auto save");
